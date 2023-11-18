cac0514b2b0cab397a436faaf3523aed73ad88c6
joa-dev <joa.neis.dev@gmail.com>
2023-11-18 19:23:47 -0300

    Spring Boot Init with Dependencies

---
+++ src/main/java/com/grupo11/demo/Grupo11Application.java
@@ -0,0 +1,13 @@
+package com.grupo11.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo11/demo/Grupo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo11.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
