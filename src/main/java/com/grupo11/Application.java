adf679b32d80611b1b42f6bb62fd75a08ccc3921
joa-dev <joa.neis.dev@gmail.com>
2023-11-18 21:04:26 -0300

    Estructura

---
+++ src/main/java/com/grupo11/Application.java
@@ -0,0 +1,10 @@
+package com.grupo11;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
