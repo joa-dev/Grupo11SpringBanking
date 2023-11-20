6a05bba9e8c81c32d190d28a14cd775962ea6aee
joa-dev <joa.neis.dev@gmail.com>
2023-11-20 16:18:34 -0300

    complete clase JPA

--- src/main/java/com/grupo11/Application.java
+++ src/main/java/com/grupo11/Application.java
@@ -1,5 +1,7 @@
 package com.grupo11;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/com/grupo11/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.grupo11.repositories;
+
+import com.grupo11.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
