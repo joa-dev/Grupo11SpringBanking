e9ae189c12c9cd22b8867cfe5a09bc323bd430c1
unknown <you@example.com>
2023-12-03 15:56:58 -0300

    agregando id a la inversión en dto

--- src/main/java/com/grupo11/mappers/InvestmentMapper.java
+++ src/main/java/com/grupo11/mappers/InvestmentMapper.java
@@ -23,10 +23,10 @@ public class InvestmentMapper {
 
     public static InvestmentDto investmentToDto(Investment investment){
         InvestmentDto dto = new InvestmentDto();
+        dto.setId(investment.getId());
         dto.setAccountId(investment.getAccount().getId());
         dto.setInvestmentInterest(investment.getInvestmentInterest());
         dto.setRetornsTime(investment.getRetornsTime());
-
         dto.setAmount(investment.getAmount());
         dto.setBalance(investment.getBalance());
 
