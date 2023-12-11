f39dd563f5a60982f6926faa50acbf173a03140b
matias9486 <matias9486mdp@gmail.com>
2023-12-10 23:30:30 -0300

    modificado el mapper para que tenga las relaciones de inversion y transferencias

--- src/main/java/com/grupo11/mappers/AccountMapper.java
+++ src/main/java/com/grupo11/mappers/AccountMapper.java
@@ -2,19 +2,35 @@ package com.grupo11.mappers;
 
 import com.grupo11.entities.Account;
 import com.grupo11.entities.dtos.AccountDto;
+import com.grupo11.entities.dtos.InvestmentDto;
+import com.grupo11.entities.dtos.TransferDto;
 import lombok.experimental.UtilityClass;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @UtilityClass
 public class AccountMapper {
     public /*static*/  AccountDto accountToDto(Account account){
         AccountDto dto = new AccountDto();
         dto.setId(account.getId());
+        dto.setUserId(account.getOwner().getId());
+        dto.setUserName(account.getOwner().getUsername());
         dto.setType(account.getType());
         dto.setCbu(account.getCbu());
         dto.setAlias(account.getAlias());
         dto.setAmount(account.getAmount());
+
+        List<InvestmentDto> investments = account.getInvestments().stream().map( i -> InvestmentMapper.investmentToDto(i)).collect(Collectors.toList());
+        dto.setInvestments(investments);
+
+        List<TransferDto> transfers = account.getTransfersSent().stream().map(t -> TransferMapper.transferToDto(t)).collect(Collectors.toList());
+        dto.setTransfersSent(transfers);
+
+        List<TransferDto> transfersReceived = account.getTransfersReceived().stream().map(t -> TransferMapper.transferToDto(t)).collect(Collectors.toList());
+        dto.setTransfersReceived(transfersReceived);
+
         dto.setCreated_at(account.getCreated_at());
         dto.setUpdated_at(account.getUpdated_at());
         return dto;
